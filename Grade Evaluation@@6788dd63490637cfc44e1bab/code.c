c7c164a2f427ac9f8e11d49f9ffee2807f23b05e
Ishpreet Singh <ishu11.22767@gmail.com>
2025-01-30 11:54:34 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -5,5 +5,21 @@ int main() {
     if(grade=='A'){
         printf("Excellent");
     }
+    else if(grade=='B'){
+        printf("Good");
+    }
+    else if(grade=='C'){
+        printf("Average");
+
+    }
+    else if(grade=='D'){
+        printf("Below Average");
+    }
+    else if(grade=='F'){
+        printf("Fail");
+    }
+    else{
+        printf("Invalid grade");
+    }
     return 0;
 }
