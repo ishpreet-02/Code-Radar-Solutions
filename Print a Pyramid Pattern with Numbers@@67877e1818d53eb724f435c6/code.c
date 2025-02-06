fea5dcf855c9ef121df044d32e5dd79d8627255b
Ishpreet Singh <ishu11.22767@gmail.com>
2025-02-07 00:07:00 +0530

    Solution to Print a Pyramid Pattern with Numbers

--- Print a Pyramid Pattern with Numbers@@67877e1818d53eb724f435c6/code.c
+++ Print a Pyramid Pattern with Numbers@@67877e1818d53eb724f435c6/code.c
@@ -3,14 +3,16 @@
 int main(){
     int n;
     scanf("%d",&n);
+    int num=1
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
             printf(" ");
 
 
         }
-        for(int k=1;k<=(2*i);k++){
-            printf("*");
+        for(int k=1;k<=i;k++){
+            printf("%d",num);
+            num++;
         }
         printf("\n");
     }
