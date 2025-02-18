f3d14223a3f30475dbc12301288fdec6932a32b3
Ishpreet Singh Bhatia <ishu11.22767@gmail.com>
2025-02-18 08:06:32 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d",&a);
     if(isalpha(a)){
         char lowercase=tolower(a);
-        if(lowercase='a'||lowercase='e'||lowercase='i'||lowercase='o'||lowercase='u'){
+        if(lowercase=='a'||lowercase=='e'||lowercase=='i'||lowercase=='o'||lowercase=='u'){
             printf("Vowel");
         }
         else{
