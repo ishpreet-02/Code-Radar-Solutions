f58e23b6472358d931e5c43f0f296aa4a69b839a
Ishpreet Singh Bhatia <ishu11.22767@gmail.com>
2025-02-24 20:12:09 +0530

    Solution to Find the First Repeating Element

--- Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
+++ Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <limits.h>
+
+#define MAX 100000  // Define a large enough max array size
 
 int main() {
     int n;
     scanf("%d", &n);
     int arr[n];
+    int index[MAX];
+
+    // Initialize index array with -1
+    for (int i = 0; i < MAX; i++) {
+        index[i] = -1;
+    }
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
+    int first_repeat_index = INT_MAX;  // Store the minimum index of repetition
+
     for (int i = 0; i < n; i++) {
-        if (arr[i] == arr[n-i]) {
-            printf("%d", arr[i]);
-            return 0;
+        if (index[arr[i]] != -1) {  // If already present
+            if (index[arr[i]] < first_repeat_index) {
+                first_repeat_index = index[arr[i]];
+            }
+        } else {
+            index[arr[i]] = i;  // Store index of first occurrence
         }
     }
 
-    printf("-1");
+    if (first_repeat_index == INT_MAX) {
+        printf("-1");  // No repeating element found
+    } else {
+        printf("%d", arr[first_repeat_index]);  // Print the first repeating element
+    }
+
     return 0;
 }
