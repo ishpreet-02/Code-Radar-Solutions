2016151f9a6bec0d2b3d4d2ccb009c98f0342e83
Ishpreet Singh Bhatia <ishu11.22767@gmail.com>
2025-02-27 10:28:15 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,37 +1,36 @@
-// Your code here...
-#include<stdio.h>
-#include <limits.h>
-void second_largest(int arr[], int n){
-    int largest=arr[0];
-    int second_largest=INT_MIN;
-    for(int i=0;i<n;i++){
-        if(arr[i]>largest){
-            second_largest=largest;
-            largest=arr[i];
-        }
-        else if (arr[i]<largest && arr[i]>second_largest){
-            second_largest=arr[i];
+#include <stdio.h>
 
-        }
-
-
-        }
-    if(second_largest==INT_MIN){
-        printf("%d",-1);
-    }
-    else{
-        printf("%d",second_largest);
-      
-    }
+void second_largest(int arr[], int n) {
+    if (n < 2) {  
+        printf("-1\n");  // If there are fewer than 2 elements, no second largest exists.
+        return;
     }
 
-int main(){
+    int largest = arr[0];
+    int second_largest = -1; // Assume no second largest initially.
+
+    for (int i = 1; i < n; i++) { 
+        if (arr[i] > largest) {
+            second_largest = largest;
+            largest = arr[i];
+        } else if (arr[i] < largest && (second_largest == -1 || arr[i] > second_largest)) {
+            second_largest = arr[i];
+        }
+    }
+
+    printf("%d\n", (second_largest == -1) ? -1 : second_largest); // Print -1 if no second largest exists.
+}
+
+int main() {
     int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    second_largest(arr,n);
+    scanf("%d", &n);
 
+    int arr[n]; // Valid in C99+
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    second_largest(arr, n);
+    return 0;
 }
