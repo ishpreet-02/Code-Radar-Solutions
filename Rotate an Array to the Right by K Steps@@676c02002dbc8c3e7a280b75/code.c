da19b74c1bd5b435ab856f6584c121d609548471
Ishpreet Singh Bhatia <ishu11.22767@gmail.com>
2025-03-31 23:27:48 +0530

    Solution to Rotate an Array to the Right by K Steps

--- Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
+++ Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
@@ -1,45 +1,34 @@
+// Your code here...
 #include <stdio.h>
+void rotate(int arr[],int n,int d){
+    int temp[d];
+    for(int i=0;i<d;i++){
+        temp[i]=arr[i];
+    }
+    
+    for(int i=d;i<n;i++){
+        arr[i-d]=arr[i];
 
-void rotate(int arr[], int n, int d) {
-    if (d == 0 || d == n) return; // No need to rotate if d is 0 or equal to n
-
-    int temp[d];  
-
-    // Store first 'd' elements in temp
-    for (int i = 0; i < d; i++) {
-        temp[i] = arr[i];
+    }
+    int j=0;
+    for(int i=n-d;i<n;i++){
+        arr[i]=temp[j];
+        j++;
     }
 
-    // Shift the remaining elements to the left
-    for (int i = d; i < n; i++) {
-        arr[i - d] = arr[i];
-    }
-
-    // Copy back the 'd' elements from temp to the end
-    for (int i = 0; i < d; i++) {
-        arr[n - d + i] = temp[i];
-    }
 }
-
-int main() {
+int main(){
     int n;
-    scanf("%d", &n);
-
+    scanf("%d",&n);
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
     int k;
-    scanf("%d", &k);
-
-    k = k % n;  // To handle cases where k >= n
-    rotate(arr, n, n-k);
-
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    scanf("%d",&k);
+    k=k%n;
+    rotate(arr,n,n-k);
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
     }
-    printf("\n");
-
-    return 0;
 }
