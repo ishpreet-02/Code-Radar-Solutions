925ce0698111c3cce25fe9462b726616ae27c6db
Ishpreet Singh Bhatia <ishu11.22767@gmail.com>
2025-03-31 21:53:55 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
-
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
+
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) { 
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                
             }
-        }
-    }
+        }}
 }
-
-int main() {
+int main(){
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n);
     int arr[n];
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+ 
+        scanf("%d",&arr[i]);
+
     }
-
-    bubbleSort(arr, n);
-
-    // Count occurrences of each unique number
-    int count = 1;  // At least one occurrence
-    for (int i = 1; i <= n; i++) {  // <= n to print last element count
-        if (i < n && arr[i] == arr[i - 1]) {
+    bubbleSort(arr,n);
+    int count = 1;  
+    for (int i = 1; i <= n; i++) {  
+        if (arr[i] == arr[i - 1]) {
             count++;
         } else {
             printf("%d %d\n", arr[i - 1], count);
-            count = 1; // Reset count for the next number
+            count = 1; 
         }
     }
-
     return 0;
+
+
 }
