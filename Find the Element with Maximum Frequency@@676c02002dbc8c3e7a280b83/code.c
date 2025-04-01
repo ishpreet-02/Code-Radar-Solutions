55ab20b440600abb8b900c97a908d53414b7ab46
Ishpreet Singh Bhatia <ishu11.22767@gmail.com>
2025-04-01 09:45:18 +0530

    Solution to Find the Element with Maximum Frequency

--- Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
+++ Find the Element with Maximum Frequency@@676c02002dbc8c3e7a280b83/code.c
@@ -13,7 +13,7 @@ void bubbleSort(int arr[],int n){
 }
 int main(){
     int n;
-    scanf("%d",&arr[i]);
+    scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
